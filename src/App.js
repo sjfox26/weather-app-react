d9c0a7b94d4b78bd7636353e2e49e0aa5087936b
Scott Fox <scottfox87@gmail.com>
2018-06-22 14:44:27 -0500

    set up initial API call with temp and description data displayed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import WeatherNow from './containers/WeatherNow/WeatherNow';
 
+console.log(process.env.REACT_APP_CURRENT_WEATHER_KEY);
+
 class App extends Component {
   render() {
     return (
--- src/containers/WeatherNow/WeatherNow.js
+++ src/containers/WeatherNow/WeatherNow.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux/Aux';
+import axios from 'axios';
+
 
 class WeatherNow extends Component {
+
+    state = {
+        weather_description: '',
+        temp: null
+    }
+
+    componentDidMount () {
+        //let url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=' + process.env.REACT_APP_CURRENT_WEATHER_KEY;
+        let url = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=' + process.env.REACT_APP_CURRENT_WEATHER_KEY;
+        axios.get(url)
+            .then(res => {
+                //console.log(res.data.city.name);
+                console.log(res.data.weather[0].description);
+                console.log(res.data.main.temp);
+
+                const newDescription = res.data.weather[0].description;
+                const newTemp = res.data.main.temp;
+                this.setState({
+                    weather_description: newDescription,
+                    temp: newTemp
+                });
+
+
+            })
+    }
 
     render () {
         return (
             <Aux>
                 <div>The Current Weather...</div>
+                <div>
+                    <p>{this.state.weather_description}</p>
+                    <p>{this.state.temp}</p>
+                    <p>{(9/5 * (this.state.temp - 273) + 32).toFixed(2)}</p>
+                </div>
             </Aux>
 
         );
