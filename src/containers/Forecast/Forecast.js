de86f46fb59b1b1e87a2320bbc4ea05a04f54f3f
Scott Fox <scottfox87@gmail.com>
2018-06-25 11:45:45 -0500

    connect with weather API in Forecast component

--- src/containers/Forecast/Forecast.js
+++ src/containers/Forecast/Forecast.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+import classes from './Forecast.css';
 
 class Forecast extends Component {
+
+
+    componentDidMount () {
+
+        let url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + 'Chicago' + '&APPID=' + process.env.REACT_APP_CURRENT_WEATHER_KEY;
+        axios.get(url)
+            .then(res => {
+                console.log(res.data);
+
+            })
+
+    }
 
     render () {
 
         return (
-            <div>
+            <div className={classes.Forecast}>
                 The 5-Day Forecast...
             </div>
         );
